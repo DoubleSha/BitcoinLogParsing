66157b1569ef78827bbd4db428402163916cf54a
Kevin Greene <kgreenek@gmail.com>
2014-08-02 17:29:23 -0700

    Adding peerstats. Prints the number of total peer connections from each unique User-Agent.

---
+++ peerstats.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+logFile="$HOME/.bitcoin/debug.log"
+declare -A userAgentCounts
+totalLogEntries=0
+
+while read line; do
+  userAgent=`awk '{print $6}' < <(echo "$line")`
+  let "userAgentCounts[$userAgent]+=1"
+  let "totalLogEntries+=1"
+done < <(cat "$logFile" | grep 'receive version message')
+
+printf "total\tpercent\t   User-Agent\n"
+for userAgent in "${!userAgentCounts[@]}"; do
+  count=${userAgentCounts[$userAgent]}
+  percent=$(bc -l <<< "$count/$totalLogEntries*100")
+  printf "%d\t%.1f%%\t   %s\n" $count $percent $userAgent
+done | sort -nr
+
